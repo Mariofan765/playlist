dd086103be278fe2efe60199269dae009f870913
Mariofan765 <mariofan7652003@yandex.ru>
2023-07-10 22:57:44 +0300

    first commit

--- index.js
+++ index.js
@@ -26,9 +26,32 @@ $(document).ready(function() {
             time: '2:29'
         }
     ]
-    let template = `<div class="popup"></div>`
+    let template = `
+        <div class="popup">
+        <input class="popup__close" type="button" value="X">
+        <div class="popup__forms">
+            
+        </div>
+        <input class="popup__submit" type="button">
+        </div>`
     let formArr = $(document).find('.header__form-btn')
     let lang = $(document).find('.header__lang-btn')
+    let body = $(document).find('body');
+    let popups = [
+        {
+            key: 'reg',
+            name: '',
+            password: '',
+            confirmPass: '',
+            submitBtnVal: 'Зарегаться'
+        },
+        {
+            key: 'log',
+            login: '',
+            password: '',
+            submitBtnVal: 'Залогаться'
+        }
+    ]
         $(lang).each((i, item) => {
             $(item).click(function() {
                 findBlock(item)
@@ -52,9 +75,36 @@ $(document).ready(function() {
         })
         $(item).addClass('active-btn')
     }
-    function choosePopup(item) {
+    function choosePopup(itemBtn) {
+        $(itemBtn).each((i, item) => {
+            $(item).on('click', () => {
+                openPopup($(item).attr(`data-form`))
+            })
+        })
 
-
+    }
+    function openPopup(keys) {
+        $(popups).each((i, item) => {
+            if(item.key === keys) {
+               $(body).html(`${$(body).html()} ${generatePopup(template, keys)}`)
+            }
+        })
+    }
+    function generatePopup (template, key) {
+        let typeForm = popups.findIndex(function (item) {
+            return item.key;
+        })
+        console.log(typeForm)
+        let forms = $(template).find('.popup__forms')
+        switch (typeForm) {
+            case typeForm = 0:
+                console.log(forms)
+            break;
+            case typeForm = 1:
+                console.log('penis')
+            break;
+        }
+        return template;
     }
     choosePopup(formArr)
 })
